76cd8d6f4abaaa6bdcb4695901ced56fd65dc1c9
Revezer <64742445+Revezer@users.noreply.github.com>
2020-06-17 15:06:00 +0300

    анимация

---
+++ js/modal.js
@@ -0,0 +1,30 @@
+var modalLink = document.querySelector(".button-form");
+var modalPopup = document.querySelector(".modal");
+var checkIn = document.querySelector(".calendar-entry")
+var modalForm = document.querySelector(".modal-form")
+var checkOut = document.querySelector(".calendar-departure")
+
+modalLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modalPopup.classList.add("modal-show");
+    checkIn.focus();
+  });
+
+modalForm.addEventListener("submit", function (evt) {
+    if (!checkIn.value || !checkOut.value) {
+      evt.preventDefault();
+      modalPopup.classList.remove("modal-error");
+      modalPopup.offsetWidth = modalPopup.offsetWidth;
+      modalPopup.classList.add("modal-error");
+    } 
+  });
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (modalPopup.classList.contains("modal-show")) {
+        evt.preventDefault();
+        modalPopup.classList.remove("modal-show");
+        modalPopup.classList.remove("modal-error");
+      }
+    }
+  });
